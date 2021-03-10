1ab2ab258e6e0dae84349883659c1b84ed9edb25
Priscilla Hamiter <phamiter253@gmail.com>
2021-03-10 07:09:58 -0800

    Updates

---
+++ component---src-pages-index-js-af6ca9dfaed00d1f1d62.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{RXBc:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),r=n.n(a),c=n("Wbzz"),o=n("Bl7J"),i=n("vrFN");t.default=function(){return r.a.createElement(o.a,null,r.a.createElement(i.a,{title:"Home"}),r.a.createElement(c.Link,{to:"/page-2/"},"Go to page 2")," ",r.a.createElement("br",null),r.a.createElement(c.Link,{to:"/using-typescript/"},'Go to "Using TypeScript"'))}}}]);
+//# sourceMappingURL=component---src-pages-index-js-af6ca9dfaed00d1f1d62.js.map
