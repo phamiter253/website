c79e0323b5d8f3f72726af18ad27a611555ad6d9
phamiter253 <34218345+phamiter253@users.noreply.github.com>
2024-12-31 15:57:25 -0800

    push

--- components/grid.vue
+++ components/grid.vue
@@ -2,36 +2,36 @@
 .grid
   .container
     .grid__container
-      .cell(style='background-image: url("/images/home-projects-24.jpg")' :class="{ 'cell--active': currIndex === 1 }" @click='setCurrent(1)')
-      .cell(style='background-image: url("/images/home-projects-21.jpg")' :class="{ 'cell--active': currIndex === 2 }" @click='setCurrent(2)')
+      .cell(style='background-image: url("https://priscillahamiter.com/images/home-projects-24.jpg")' :class="{ 'cell--active': currIndex === 1 }" @click='setCurrent(1)')
+      .cell(style='background-image: url("https://priscillahamiter.com/images/home-projects-21.jpg")' :class="{ 'cell--active': currIndex === 2 }" @click='setCurrent(2)')
       .cell-large
         h2.cell__title Upcoming Project
       .cell-large
         h2.cell__title Upcoming Project
-      .cell(style='background-image: url("/images/home-projects-22.jpg")' :class="{ 'cell--active': currIndex === 3 }" @click='setCurrent(3)')
-      .cell(style='background-image: url("/images/home-projects-23.jpg")' :class="{ 'cell--active': currIndex === 4 }" @click='setCurrent(4)')
-      .cell(style='background-image: url("/images/home-projects-20.jpg")' :class="{ 'cell--active': currIndex === 5 }" @click='setCurrent(5)')
-      .cell(style='background-image: url("/images/home-projects-19.jpg")' :class="{ 'cell--active': currIndex === 6 }" @click='setCurrent(6)')
+      .cell(style='background-image: url("https://priscillahamiter.com/images/home-projects-22.jpg")' :class="{ 'cell--active': currIndex === 3 }" @click='setCurrent(3)')
+      .cell(style='background-image: url("https://priscillahamiter.com/images/home-projects-23.jpg")' :class="{ 'cell--active': currIndex === 4 }" @click='setCurrent(4)')
+      .cell(style='background-image: url("https://priscillahamiter.com/images/home-projects-20.jpg")' :class="{ 'cell--active': currIndex === 5 }" @click='setCurrent(5)')
+      .cell(style='background-image: url("https://priscillahamiter.com/images/home-projects-19.jpg")' :class="{ 'cell--active': currIndex === 6 }" @click='setCurrent(6)')
       .cell-large
         h2.cell__title Upcoming Project
-      .cell(style='background-image: url("/images/home-projects-18.jpg")' :class="{ 'cell--active': currIndex === 7 }" @click='setCurrent(7)')
-      .cell(style='background-image: url("/images/home-projects-17.jpg")' :class="{ 'cell--active': currIndex === 8 }" @click='setCurrent(8)')
-      .cell(style='background-image: url("/images/home-projects-16.jpg")' :class="{ 'cell--active': currIndex === 9 }" @click='setCurrent(9)')
-      .cell(style='background-image: url("/images/home-projects-15.jpg")' :class="{ 'cell--active': currIndex === 10 }" @click='setCurrent(10)')
-      .cell(style='background-image: url("/images/home-projects-14.jpg")' :class="{ 'cell--active': currIndex === 11 }" @click='setCurrent(11)')
-      .cell(style='background-image: url("/images/home-projects-13.jpg")' :class="{ 'cell--active': currIndex === 12 }" @click='setCurrent(12)')
-      .cell(style='background-image: url("/images/home-projects-12.jpg")' :class="{ 'cell--active': currIndex === 13 }" @click='setCurrent(13)')
-      .cell(style='background-image: url("/images/home-projects-11.jpg")' :class="{ 'cell--active': currIndex === 14 }" @click='setCurrent(14)')
-      .cell(style='background-image: url("/images/home-projects-10.jpg")' :class="{ 'cell--active': currIndex === 15 }" @click='setCurrent(15)')
-      .cell(style='background-image: url("/images/home-projects-9.jpg")' :class="{ 'cell--active': currIndex === 16 }" @click='setCurrent(16)')
-      .cell(style='background-image: url("/images/home-projects-8.jpg")' :class="{ 'cell--active': currIndex === 17 }" @click='setCurrent(17)')
-      .cell(style='background-image: url("/images/home-projects-7.jpg")' :class="{ 'cell--active': currIndex === 18 }" @click='setCurrent(18)')
-      .cell(style='background-image: url("/images/home-projects-6.jpg")' :class="{ 'cell--active': currIndex === 19 }" @click='setCurrent(19)')
-      .cell(style='background-image: url("/images/home-projects-5.jpg")' :class="{ 'cell--active': currIndex === 20 }" @click='setCurrent(20)')
-      .cell(style='background-image: url("/images/home-projects-4.jpg")' :class="{ 'cell--active': currIndex === 21 }" @click='setCurrent(21)')
-      .cell(style='background-image: url("/images/home-projects-3.jpg")' :class="{ 'cell--active': currIndex === 22 }" @click='setCurrent(22)')
-      .cell(style='background-image: url("/images/home-projects-2.jpg")' :class="{ 'cell--active': currIndex === 23 }" @click='setCurrent(23)')
-      .cell(style='background-image: url("/images/home-projects-1.jpg")' :class="{ 'cell--active': currIndex === 24 }" @click='setCurrent(24)')
+      .cell(style='background-image: url("https://priscillahamiter.com/images/home-projects-18.jpg")' :class="{ 'cell--active': currIndex === 7 }" @click='setCurrent(7)')
+      .cell(style='background-image: url("https://priscillahamiter.com/images/home-projects-17.jpg")' :class="{ 'cell--active': currIndex === 8 }" @click='setCurrent(8)')
+      .cell(style='background-image: url("https://priscillahamiter.com/images/home-projects-16.jpg")' :class="{ 'cell--active': currIndex === 9 }" @click='setCurrent(9)')
+      .cell(style='background-image: url("https://priscillahamiter.com/images/home-projects-15.jpg")' :class="{ 'cell--active': currIndex === 10 }" @click='setCurrent(10)')
+      .cell(style='background-image: url("https://priscillahamiter.com/images/home-projects-14.jpg")' :class="{ 'cell--active': currIndex === 11 }" @click='setCurrent(11)')
+      .cell(style='background-image: url("https://priscillahamiter.com/images/home-projects-13.jpg")' :class="{ 'cell--active': currIndex === 12 }" @click='setCurrent(12)')
+      .cell(style='background-image: url("https://priscillahamiter.com/images/home-projects-12.jpg")' :class="{ 'cell--active': currIndex === 13 }" @click='setCurrent(13)')
+      .cell(style='background-image: url("https://priscillahamiter.com/images/home-projects-11.jpg")' :class="{ 'cell--active': currIndex === 14 }" @click='setCurrent(14)')
+      .cell(style='background-image: url("https://priscillahamiter.com/images/home-projects-10.jpg")' :class="{ 'cell--active': currIndex === 15 }" @click='setCurrent(15)')
+      .cell(style='background-image: url("https://priscillahamiter.com/images/home-projects-9.jpg")' :class="{ 'cell--active': currIndex === 16 }" @click='setCurrent(16)')
+      .cell(style='background-image: url("https://priscillahamiter.com/images/home-projects-8.jpg")' :class="{ 'cell--active': currIndex === 17 }" @click='setCurrent(17)')
+      .cell(style='background-image: url("https://priscillahamiter.com/images/home-projects-7.jpg")' :class="{ 'cell--active': currIndex === 18 }" @click='setCurrent(18)')
+      .cell(style='background-image: url("https://priscillahamiter.com/images/home-projects-6.jpg")' :class="{ 'cell--active': currIndex === 19 }" @click='setCurrent(19)')
+      .cell(style='background-image: url("https://priscillahamiter.com/images/home-projects-5.jpg")' :class="{ 'cell--active': currIndex === 20 }" @click='setCurrent(20)')
+      .cell(style='background-image: url("https://priscillahamiter.com/images/home-projects-4.jpg")' :class="{ 'cell--active': currIndex === 21 }" @click='setCurrent(21)')
+      .cell(style='background-image: url("https://priscillahamiter.com/images/home-projects-3.jpg")' :class="{ 'cell--active': currIndex === 22 }" @click='setCurrent(22)')
+      .cell(style='background-image: url("https://priscillahamiter.com/images/home-projects-2.jpg")' :class="{ 'cell--active': currIndex === 23 }" @click='setCurrent(23)')
+      .cell(style='background-image: url("https://priscillahamiter.com/images/home-projects-1.jpg")' :class="{ 'cell--active': currIndex === 24 }" @click='setCurrent(24)')
 </template>
 
 <script setup>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,32 +1,29 @@
 export default defineNuxtConfig({
-  app: {
-    baseURL: '/website/',
-	},
-  components: [
-    {
-      path: '~/components',
-      pathPrefix: false,
-    },
-  ],
-  ssr: true,
-  nitro: {
-    prerender: {
-      crawlLinks: true,
-      failOnError: false, 
-    },
-  },
-  routeRules: {
-    "/**": { swr: true },
-    "/dashboard/**": { ssr: false },
-  },
-  // vite: {
-  //   css: {
-  //     preprocessorOptions: {
-  //       sass: {
-  //         additionalData:  '@import "~/assets/styles/global.sass"'
-  //       }
-  //     }
-  //   }
-  // },
-  css: ['~/assets/styles/scss/main.scss']
+ app: {
+   baseURL: '/website/',
+   },
+
+ components: [
+   {
+     path: '~/components',
+     pathPrefix: false,
+   },
+ ],
+
+ ssr: true,
+
+ nitro: {
+   prerender: {
+     crawlLinks: true,
+     failOnError: false, 
+   },
+ },
+
+ routeRules: {
+   "/**": { swr: true },
+   "/dashboard/**": { ssr: false },
+ },
+
+ css: ['~/assets/styles/scss/main.scss'],
+ compatibilityDate: '2024-12-31'
 })
