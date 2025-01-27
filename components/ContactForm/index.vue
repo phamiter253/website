165e35f229bf66bd92715e576ea0d42202a688b7
phamiter253 <34218345+phamiter253@users.noreply.github.com>
2025-01-26 23:25:18 -0800

    fixing form

--- components/ContactForm/index.vue
+++ components/ContactForm/index.vue
@@ -1,16 +1,37 @@
-<script setup>
+<script>
   import axios from 'axios';
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+  export default {
+    name: 'ContactForm',
+    data() {
+      return {
+        form: {
+          name: "",
+          email: "",
+          message: "",
+        },
+      };
+    },
+    methods: {
+      handleSubmit: async function() {
+        const formData = new FormData();
 
-    await axios
-      .post("https://formeezy.com/api/v1/forms/67972c7758208a0008474679/submissions", new FormData(e.target))
-      .then(({ data }) => {
-        const { redirect } = data;
-        window.location.href = redirect;
-      });
-  }
+        for (const [key, value] of Object.entries(this.form)) {
+          formData.append(key, value);
+        }
+
+        await axios
+          .post("https://formeezy.com/api/v1/forms/67972c7758208a0008474679/submissions", formData)
+          .then(({ data }) => {
+            const { redirect } = data;
+            window.location.href = redirect;
+          })
+          .catch((e) => {
+            window.location.href = e.response.data.redirect;
+          });
+      }
+    }
+  };
 </script>
 
 <template lang="pug">
@@ -19,22 +40,20 @@
       .contact-form__column
         h1.contact-form__title Get In<br>Touch!
       .contact-form__column
-        form.contact-form__form(@submit="handleSubmit")
+        form.contact-form__form(@submit.prevent="handleSubmit")
           .contact-form__group
-            label.contact-form__label(for="name" class="form-label") Name
-            input(id="name" name="name" type="text" required)
+            label.contact-form__label(for="name" class="form-label") Name*
+            input(id="name" v-model="form.name" type="text" name="name" required)
           .contact-form__group
-            label.contact-form__label(for="email" class="form-label") Email
-            input(id="email" name="email" type="text" required)
+            label.contact-form__label(for="email" class="form-label") Email*
+            input(id="email" v-model="form.email" type="email" name="email" required)
           .contact-form__group
             label.contact-form__label(for="phone" class="form-label") Phone
             input(id="phone" name="phone" type="text")
           .contact-form__group
-            label.contact-form__label(for="message" class="form-label") Message
-            textarea(id="message" name="message")
-          //input(type="file" name="my-file")
-          input(name="bot-field" type="text" style="display: none;")
-        button.contact-form__submit(type="submit") Send Message
+            label.contact-form__label(for="message" class="form-label") Message*
+            textarea(id="message" v-model="form.message" name="message" required)
+          button.contact-form__submit(type="submit") Send Message
 </template>
 
 <style lang="sass" src="./index.sass"></style> 
